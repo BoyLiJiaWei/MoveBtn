138619f89ea9dc3bf23ebe7909debaec7273bfc2
上海钢猫有限公司 <15221536732@163.com>
2016-09-09 09:26:33 +0800

    first commit

--- moveBtn/ViewController.m
+++ moveBtn/ViewController.m
@@ -9,19 +9,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIButton *moveBtn;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self testMove];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)testMove {
+    moveBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    moveBtn.frame = CGRectMake([UIScreen mainScreen].bounds.size.width/2-30, [UIScreen mainScreen].bounds.size.height/2-30, 60, 60);
+    moveBtn.backgroundColor = [UIColor redColor];
+    moveBtn.layer.cornerRadius = 30;
+    moveBtn.layer.masksToBounds = YES;
+    [self.view addSubview:moveBtn];
+    
+    [moveBtn addTarget:self action:@selector(dragMoving:withEvent: )forControlEvents: UIControlEventTouchDragInside];
+//    [moveBtn addTarget:self action:@selector(doClick:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void) dragMoving: (UIButton *)btn withEvent:(UIEvent *)event {
+    
+    CGPoint point = [[[event allTouches] anyObject] locationInView:self.view];
+    CGFloat x = point.x;
+    CGFloat y = point.y;
+    CGFloat btnx = btn.frame.size.width/2;
+    CGFloat btny = btn.frame.size.height/2;
+
+    if(x<=btnx) {
+        point.x = btnx;
+    }
+    if(x >= self.view.bounds.size.width - btnx) {
+        point.x = self.view.bounds.size.width - btnx;
+    }
+    
+    NSLog(@"fs:%f %f",x, btnx);
+    btn.center = point;
+    NSLog(@"%f,%f",btn.center.x,btn.center.y);
 }
 
 @end
